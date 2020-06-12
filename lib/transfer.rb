c5718e6c65d84445bab268a0de2683d19507ec4e
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-06-12 20:10:25 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,8 +20,7 @@ class Transfer
   def execute_transaction
      
     
-    if sender.balance > @amount && @status == "pending" && sender.valid? == true && sender.status != "closed"
-      
+    if sender.balance > @amount && @status == "pending" 
       sender.balance -= @amount
       receiver.balance += @amount
       @status = "complete"
