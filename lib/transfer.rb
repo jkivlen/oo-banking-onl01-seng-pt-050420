89ba454e5bbed463a15841843af5828f94430998
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-06-12 20:18:43 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -31,7 +31,7 @@ class Transfer
   def reverse_transfer
     if @status == "complete"
       sender.balance += @amount
-      @receiver.balance -= @amount
+      receiver.balance -= @amount
       @status = "reversed"
     end
   end
